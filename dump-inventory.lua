834fcefbeef64043ae7aaef5ec26150925bbe056
Aiden McClelland <me@drbonez.dev>
2025-04-30 15:44:56 -0600

    update dump-inventory

--- dump-inventory.lua
+++ dump-inventory.lua
@@ -23,10 +23,25 @@ local function dumpInventory()
     if not data["tags"]["c:chests"] then
         return false
     end
+    local fuelStacks = 0
+    local torchStacks = 0
     for slot = 1, 16 do
-        if getFuelValue(slot) == 0 and not isTorch(slot) then
+        local target = 'up'
+        if getFuelValue(slot) ~= 0 and fuelStacks < 3 then
+            fuelStacks = fuelStacks + 1
+            target = 'down'
+        elseif isTorch(slot) and torchStacks < 3 then
+            torchStacks = torchStacks + 1
+            target = 'down'
+        else
             turtle.select(slot)
-            if not turtle.dropUp() then
+            local res = false
+            if target == 'up' then
+                res = turtle.dropUp()
+            elseif target == 'down' then
+                res = turtle.dropDown()
+            end
+            if not res then
                 return false
             end
         end
